d426a4d54435d13fe8c4b2a6aa28cdc749d1b50f
AcuteChedder <nubik8653@gmail.com>
2025-05-27 21:26:16 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Header from './components/Header.vue'
+</script>
+
+<template>
+  <div class=" mx-6 mt-8 w-60">
+    <Header />
+
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class=" flex gap-60">
+        <img src="../assets/BurgerLogo.png" alt="" >
+        <div class=" flex gap-16 items-center justify-center">
+            <p class=" font-Sansita text-white">Home</p>
+            <p class=" font-Sansita text-white">Menu</p>
+            <p class=" font-Sansita text-white">About</p>
+            <p class=" font-Sansita text-white">Shop</p>
+        </div>
+    </div>
+</template>
